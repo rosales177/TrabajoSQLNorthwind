39db56bc198f2f697443c461e771ddb64e482438
Geraldo Oneil Rosales Poma <rosalespoma15@gmail.com>
2021-09-02 22:01:49 -0500

    Proc update Employees terminado

--- TrabajoSQLNorthwind.sql
+++ TrabajoSQLNorthwind.sql
@@ -8,22 +8,148 @@ DROP PROC IF EXISTS sp_Update_Employees
 GO
 
 CREATE PROC sp_Update_Employees
-@EmployeeID int not null,
-@LastName nvarchar(20) not null,
-@FirstName nvarchar(10) not null,
-@Title nvarchar(30) null,
-@TitleOfCourtesy nvarchar(25) null,
-@BirthDate date null,
-@HireDate date null,
-@Address nvarchar(60) null,
-@City nvarchar(15) null,
-@Region nvarchar(15) null,
-@PostalCode nvarchar(10) null,
-@Country nvarchar(15) null,
-@HomePhone nvarchar(24) null,
-@Extension nvarchar(4) null
+@EmployeeID int,
+@LastName nvarchar(20) ,
+@FirstName nvarchar(10) ,
+@Title nvarchar(30) ,
+@TitleOfCourtesy nvarchar(25) ,
+@BirthDate date ,
+@HireDate date ,
+@Address nvarchar(60) ,
+@City nvarchar(15) ,
+@Region nvarchar(15) ,
+@PostalCode nvarchar(10) ,
+@Country nvarchar(15) ,
+@HomePhone nvarchar(24) ,
+@Extension nvarchar(4),
+@Photo image,
+@Notes ntext,
+@ReportsTo int,
+@PhotoPath nvarchar(255)
 as
+	SET NOCOUNT ON;
+	DECLARE @Mensaje nvarchar(100); 
+	IF (@EmployeeID is null or @EmployeeID = 0)
+	BEGIN
+		SET @Mensaje = 'Error en la variable @EmployeeID, fuera de rango o nulo'
+		PRINT @Mensaje
+		RETURN
+	END
+	IF (@LastName is null or LEN(@LastName)=0)
+	BEGIN
+		SET @Mensaje = 'Error en la variable @LastName, fuera de rango o nulo'
+		PRINT @Mensaje
+		RETURN
+	END
+	IF (@FirstName is null or LEN(@FirstName)=0)
+	BEGIN
+		SET @Mensaje = 'Error en la variable @FirstName, fuera de rango o nulo'
+		PRINT @Mensaje
+		RETURN
+	END
+	IF (LEN(@Title)>=31)
+	BEGIN
+		SET @Mensaje = 'Error en la variable @Title, fuera de rango.'
+		PRINT @Mensaje
+		RETURN
+	END
+	IF (LEN(@TitleOfCourtesy)>=26)
+	BEGIN
+		SET @Mensaje = 'Error en la variable @TitleOfCourtesy, fuera de rango.'
+		PRINT @Mensaje
+		RETURN
+	END
+	IF (LEN(@Address)>=61)
+	BEGIN
+		SET @Mensaje = 'Error en la variable @Address, fuera de rango.'
+		PRINT @Mensaje
+		RETURN
+	END
+	IF (LEN(@City)>=16)
+	BEGIN
+		SET @Mensaje = 'Error en la variable @City, fuera de rango.'
+		PRINT @Mensaje
+		RETURN
+	END
+	IF (LEN(@Region)>=16)
+	BEGIN
+		SET @Mensaje = 'Error en la variable @Region, fuera de rango.'
+		PRINT @Mensaje
+		RETURN
+	END
+	IF (LEN(@PostalCode)>=11)
+	BEGIN
+		SET @Mensaje = 'Error en la variable @PostalCode, fuera de rango.'
+		PRINT @Mensaje
+		RETURN
+	END
+	IF (LEN(@Country)>=16)
+	BEGIN
+		SET @Mensaje = 'Error en la variable @Country, fuera de rango.'
+		PRINT @Mensaje
+		RETURN
+	END
+	IF (LEN(@HomePhone)>=25)
+	BEGIN
+		SET @Mensaje = 'Error en la variable @HomePhone, fuera de rango.'
+		PRINT @Mensaje
+		RETURN
+	END
+	IF (LEN(@Extension)>=5)
+	BEGIN
+		SET @Mensaje = 'Error en la variable @Extension, fuera de rango.'
+		PRINT @Mensaje
+		RETURN
+	END
+	IF (@ReportsTo is null or @ReportsTo = 0)
+	BEGIN
+		SET @Mensaje = 'Error en la variable @RepostsTo, fuera de rango o nulo.'
+		PRINT @Mensaje
+		RETURN
+	END
+	IF (LEN(@PhotoPath)>=255)
+	BEGIN
+		SET @Mensaje = 'Error en la variable @PhotoPath, fuera de rango.'
+		PRINT @Mensaje
+		RETURN
+	END
+	BEGIN TRAN
+		BEGIN TRY
+			SET @Mensaje = 'El registro se actualizó correctamente.'
+			UPDATE Employees SET
+			[LastName] = @LastName,
+			[FirstName] = @FirstName,
+			[Title] = @Title,
+			[TitleOfCourtesy] = @TitleOfCourtesy,
+			[BirthDate] = @BirthDate,
+			[HireDate] = @HireDate,
+			[Address] = @Address,
+			[City] = @City,
+			[Region] = @Region,
+			[PostalCode] = @PostalCode,
+			[Country] = @Country,
+			[HomePhone] = @HomePhone,
+			[Extension] = @Extension,
+			[Photo] = @Photo,
+			[Notes] = @Notes,
+			[ReportsTo] = @ReportsTo,
+			[PhotoPath] = @PhotoPath
+			WHERE 
+			[EmployeeID] = @EmployeeID
+			PRINT @Mensaje
+			COMMIT TRAN
+		END TRY
+		BEGIN CATCH
+			ROLLBACK TRAN
+			SET @Mensaje = 'Error en la transacción : '
+			PRINT @Mensaje
+			SELECT ERROR_MESSAGE() as ErrorMenssage
+		END CATCH
+
 GO
+
+EXEC sp_Update_Employees 9,'Dodsworth','Annie','Sales Representative','Ms.','1966-01-27','2000-11-15','7 Houndstooth Rd','London','WA','WG2 7LT','UK','(71)555-4443','4555555',null,'Anne has a BA degree in English from St. Lawrence College.  She is fluent in French and German.',5,null
+go
 
 
 -----------------------------------Sección Fiorella-----------------------------------------------
